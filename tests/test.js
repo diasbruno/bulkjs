d45640968a63a5f501201bf33162cdeb3b26f83b
Bruno Dias <dias.h.bruno@gmail.com>
2016-03-22 22:35:07 -0300

    added tests for mark().

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,21 @@
-var Bulk = require('../bulk.js');
+var Bulk = require("../bulk.js");
+
+var data = [
+  {uuid: 1, name: "test a"},
+  {uuid: 2, name: "test b"},
+  {uuid: 3, name: "test c"},
+  {uuid: 4, name: "test d"},
+  {uuid: 5, name: "test e"}
+];
+
+describe("Bulk", function() {
+  it("#mark(uuid)", function() {
+    var bulk = Bulk({}, {
+      toId: function(x) {
+        return x.uuid;
+      }
+    });
+    bulk.mark(1);
+    bulk.list().should.be.eql([1]);
+  });
+});
